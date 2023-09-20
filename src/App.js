5c4a73c554e306eed8ac179e8ccfba0577d7f6d0
MuhammadAhmed1994 <amallick@folio3.com>
2023-09-20 22:14:46 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>src/App.js</code> and save to reload.
+          {process.env.REACT_APP_TESTER} <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
